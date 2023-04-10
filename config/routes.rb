d08816e7d8b619b127b48280756a418290ecde16
hugostanley <stanley.hugo@edufied.edu.vn>
2023-04-10 10:13:47 +0800

    chore(routes): add routes for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,22 @@
 Rails.application.routes.draw do
+  get 'user/login', to: "sessions#new", as: "user_login"
+  post 'sessions/create', to: "sessions#create", as: "sessions" 
+  delete 'sessions/destroy', to: "sessions#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "users#index"
   get "/index", to: "application#index"
+
+  # all users
   get "/users", to: "users#index", as: "all_users"
+
+  # user signup form
   get "/user/signup", to: "users#new", as: "new_user"
+
+  # user post request signup
   post "/user/signup", to: "users#create"
-  get "users/:id", to: "users#show", as: "user"
+
+  # user profile
+  get "users/profile/:id", to: "users#show", as: "user"
 end
