91df49bcf61fa7b992e69b4adb269da4ae5251f0
hugostanley <stanley.hugo@edufied.edu.vn>
2023-04-10 10:13:25 +0800

    chore(auth): make the model use has_secure_password and password digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
+require 'bcrypt'
 class User < ApplicationRecord
+  has_secure_password
   validates :username, presence: true, length: { in: 2..50 }, uniqueness: true
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
-  validates :password, confirmation: true, presence: true, length: { minimum: 8 }
-  validates :password_confirmation, presence: true
+
+  def auth_with_password_digest(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
 end
