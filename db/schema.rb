d8d3c5accd3aab8e4c161c070c33ace081fce2ac
hugostanley <stanleyhugo06@gmail.com>
2023-04-11 23:01:10 +0800

    chore(): create files generated by rails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_09_130759) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_10_121421) do
+  create_table "tasks", force: :cascade do |t|
+    t.text "title"
+    t.text "description"
+    t.datetime "deadline"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
@@ -19,4 +29,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_09_130759) do
     t.string "password_digest"
   end
 
+  add_foreign_key "tasks", "users"
 end
