c6fcb496b6c8847c2ecedd052b89bf59e3203620
hugostanley <stanleyhugo06@gmail.com>
2023-04-14 23:05:17 +0800

    chore(routes): add friendship routes and protect post routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,43 +1,50 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'landing_page/index', to: 'landing_page#index', as: 'home_page'
-  get 'user/login', to: 'sessions#new', as: 'user_login'
-  post 'sessions/create', to: 'sessions#create', as: 'sessions' 
-  delete 'sessions/destroy', to: 'sessions#destroy', as: 'user_signout'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ('/')
+  # root route
   root 'landing_page#index'
+
+  # landing page controller routes
+  get 'landing_page/index', to: 'landing_page#index', as: 'home_page'
+
+  # session controller actions: login and signout
+  get 'user/login', to: 'sessions#new', as: 'user_login'
+  post 'sessions/create', to: 'sessions#create', as: 'sessions'
+  delete 'sessions/destroy', to: 'sessions#destroy', as: 'user_signout'
 
   get '/index', to: 'application#index'
 
-  # all users
+  # USERS CONTROLLER ROUTES
+  ## all users
   get '/users', to: 'users#index', as: 'all_users'
-
-  # user signup form
+  ## user signup form
   get '/user/signup', to: 'users#new', as: 'new_user'
-
-  # user post request signup
+  ## user submit signup form
   post '/user/signup', to: 'users#create'
-
-  # user profile
+  ## user profile
   get 'users/:id/profile', to: 'users#show', as: 'user_profile'
 
-  # tasks
-
-  # all tasks
+  # TASKS CONTROLLER ROUTES
+  ## all tasks
   get 'users/:id/tasks', to: 'tasks#index', as: 'user_tasks'
-  # submit new task
-  post 'users/:id/tasks', to: 'tasks#create'
-  # new task form
+  # task signup form
   get 'users/:id/tasks/new', to: 'tasks#new', as: 'new_task'
-  # show task
+  ## task submit signup form
+  post 'users/tasks/new', to: 'tasks#create', as: 'create_task'
+  # task profile
   get 'users/:id/tasks/:task_id', to: 'tasks#show', as: 'user_task'
-
   # complete task
-  post 'users/:id/tasks/:task_id/complete', to: 'tasks#complete_task', as: 'complete_task'
-
+  post 'users/tasks/complete', to: 'tasks#complete_task', as: 'complete_task'
   # delete task
   delete 'users/:id/tasks/:task_id/delete', to: 'tasks#destroy', as: 'delete_task'
+
+  # FRIENDS CONTROLLER ROUTES
+  ## all friends
+  get 'users/:id/friends', to: 'friends#index', as: 'all_friends'
+  ## send friend request
+  post 'users/friends/add', to: 'friends#create', as: 'add_friend'
+  ## accept friend request
+  put 'users/:id/friends', to: 'friends#accept_friend_request', as: 'accept_friend_request'
+  ## reject friend request
+  put 'users/:id/friends', to: 'friends#reject_friend_request', as: 'reject_friend_request'
 end
