87a23967514270b141b842716eed8c51ba9e3ffe
hugostanley <stanley.hugo@edufied.edu.vn>
2023-04-16 15:25:00 +0800

    feat: add model methods

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
-  validates :title, presence: true, length: { in: 2..20 }
+  validates :title, presence: true, length: { in: 2..30 }
   validates :description, allow_blank: true, length: { maximum: 200 }
   validates :deadline, inclusion: {
     in: (Date.today..Float::INFINITY), message: "Deadline must be today or in the future" }, allow_blank: true, on: :create
@@ -9,6 +9,24 @@ class Task < ApplicationRecord
 
   def self.sort_tasks_by_latest_update(id)
     Task.where(user_id: id).sort_by { |task| task.updated_at }.reverse
+  end
+
+  # to get tasks
+  def self.tasks_for_feed(user)
+    friends = User.get_friends_by_status(user.id, "accepted")
+    tasks = []
+
+    friends.each do |friend|
+      friend.tasks.each do |task|
+        tasks << { username: friend.username, task: task }
+      end
+    end
+
+    user.tasks.each do |task|
+      tasks << { username: user.username, task: task }
+    end
+
+    tasks.sort_by { |task| task[:task].updated_at }.reverse
   end
 
   private 
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,17 +14,20 @@ class User < ApplicationRecord
     BCrypt::Password.new(password_digest).is_password?(password)
   end
 
-  def all_friends
-    self.friends 
+  def self.get_friends_by_status(id, status)
+    friendship_by_status = Friendship.where(user_id: id, status: status)
+    friendship_by_status.map { |f| User.find(f.friend_id) }
   end
 
-  def self.all_pending_friends(id)
-    pending_friendships = Friendship.where(user_id: id, status: 'pending')
-    pending_friends = pending_friendships.map {|f| User.find(f.friend_id)}
+  def self.get_non_friend_users(current_user)
+    User.where.not(id: current_user.id).reject { |user| current_user.friends.include?(user) }
   end
 
-  def self.all_accepted_friends(id)
-    accepted_friendships = Friendship.where(user_id: id, status: 'accepted')
-    accepted_friends = accepted_friendships.map {|f| User.find(f.friend_id)}
+  def self.get_all_users_non_friend_and_rejected_friendship_users(id)
+    current_user = User.find(id)
+    non_friend_users = User.get_non_friend_users current_user
+    rejected_friends = User.get_friends_by_status(id, 'rejected')
+    non_friend_users + rejected_friends
   end
+
 end
