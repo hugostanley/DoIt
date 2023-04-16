02afbb0e9a1ca91c7ec51fe399c5ec30ab329418
hugostanley <stanleyhugo06@gmail.com>
2023-04-16 15:25:49 +0800

    refactor(): make controller methods thinner

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,8 +1,13 @@
-class FriendsController < ApplicationController
+# frozen_string_literal: true
 
+# FriendsController
+# Controller for the friendship model
+class FriendsController < ApplicationController
   def index
-    @pending_friends = User.all_pending_friends session[:user_id]
-    @accepted_friends = User.all_accepted_friends session[:user_id]
+    @pending_friends = User.get_friends_by_status session[:user_id], 'pending'
+    @accepted_friends = User.get_friends_by_status session[:user_id], 'accepted'
+    @rejected_friends = User.get_friends_by_status session[:user_id], 'rejected'
+    @requested_friends = User.get_friends_by_status session[:user_id], 'requested'
   end
 
   def new
@@ -10,23 +15,43 @@ class FriendsController < ApplicationController
   end
 
   def create
-    @user_1_friendship = Friendship.new(private_friendship_params)
-    @user_2_friendship = Friendship.new(user_id: private_friendship_params[:friend_id], friend_id: private_friendship_params[:user_id], status: private_friendship_params[:status])
+    @user_1_friendship = Friendship.find_or_initialize_by(friend_request_params)
+    @user_2_friendship = Friendship.find_or_initialize_by(user_id: friend_request_params[:friend_id], friend_id: friend_request_params[:user_id])
+    @user_1_friendship.status = 'requested'
+    @user_2_friendship.status = 'pending'
 
     if @user_1_friendship.save && @user_2_friendship.save
-      redirect_to all_friends_path session[:user_id]
+      redirect_to all_friends_path
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def accept_friend_request
-    @user_1_friendship = Friendship.find(user_id: session[:user_id])
+    @user_1_friendship = Friendship.find_by(friend_request_params)
+    @user_2_friendship = Friendship.find_by(user_id: friend_request_params[:friend_id])
+
+    return unless @user_1_friendship.update(status: 'accepted') && @user_2_friendship.update(status: 'accepted')
+
+    redirect_to all_friends_path
+  end
+
+  def reject_friend_request
+    @user_1_friendship = Friendship.find_by(friend_request_params)
+    @user_2_friendship = Friendship.find_by(user_id: friend_request_params[:friend_id], friend_id: friend_request_params[:user_id])
+
+    return unless @user_1_friendship.update(status: 'rejected') && @user_2_friendship.update(status: 'rejected')
+
+    redirect_to all_friends_path
   end
 
   private
 
   def private_friendship_params
     params.permit(:user_id, :friend_id, :status)
+  end
+
+  def friend_request_params
+    params.require(:friendship).permit(:user_id, :friend_id)
   end
 end
--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,4 +1,10 @@
+# frozen_string_literal: true
+
+# LandingPageController
+# Pages related to landing page.
+# These are the public pages
 class LandingPageController < ApplicationController
   def index
+    redirect_to user_tasks_feed_path if session[:is_logged_in] == true
   end
 end
