bfc0b325612d7a443e7a1eba36c2cb723269ff81
hugostanley <stanley.hugo@edufied.edu.vn>
2023-04-16 15:25:22 +0800

    feat(friendship): add requested as friendship status

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,7 +3,7 @@ class Friendship < ApplicationRecord
   belongs_to :friend, class_name: 'User'
 
   validates :user_id, :friend_id , presence: true, numericality: { only_integer: true }
-  validates :status, inclusion: { in: %w(accepted rejected pending), message: "%{value} is not a valid status" }
+  validates :status, inclusion: { in: %w(accepted rejected pending requested), message: "%{value} is not a valid status" }
 
   def friend_id_should_not_be_equal_to_current_user_id
     if friend_id == session[:user_id]
